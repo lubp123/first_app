11f3ba1c8291829e4c78b906cc03567be7860de0
uma p <uma_p@server>
2012-08-13 15:35:15 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'c8be36eb542862d550ce9157621acd6abe6f65fcb0d2c0bd6fcc3d0a67596b20e111733f347055d5e77b74a682e23d449d75b1fd791f0fa931f5e42b77a7f5a6'
